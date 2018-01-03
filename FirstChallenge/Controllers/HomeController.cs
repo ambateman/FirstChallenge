beece4acc87cc28d5711a834241ca2f9a1c02244
ambateman <tmbateman@hotmail.com>
2018-01-02 19:07:48 -0800

    Add project files.

---
+++ FirstChallenge/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using FirstChallenge.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstChallenge.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+           var comix= ComicBookManager.GetComicBooks();
+            return View(comix);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var comics = ComicBookManager.GetComicBooks();
+            var comic = comics.FirstOrDefault(P => P.ComicBookId == id);
+
+
+            return View(comic);
+
+        }
+
+
+
+    }
+}
